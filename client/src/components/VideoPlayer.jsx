e2dd9a0b6f3ec5a3af7829f68c380639178a8f66
sosualfred <sosualfred@gmail.com>
2021-05-15 16:58:15 +0000

    server and client setup

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoPlayer = () => {
+    return (
+        <div>
+            Video Player
+            
+        </div>
+    )
+}
+
+export default VideoPlayer;
